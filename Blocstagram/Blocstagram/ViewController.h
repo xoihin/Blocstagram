1d4164952070f70f73f333079daf1e6275556063
Xoi Hin <xoihin@yahoo.com>
2015-08-25 11:14:03 -0400

    Initial commit

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Xoi's iMac on 2015-08-25.
+//  Copyright (c) 2015 XoiAHin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Blocstagram
+//
+//  Created by Xoi's iMac on 2015-08-25.
+//  Copyright (c) 2015 XoiAHin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
