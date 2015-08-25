e45060e57dd019a76d1d20176b691888a8ec8c83
Xoi Hin <xoihin@yahoo.com>
2015-08-25 12:22:02 -0400

    Checkpoint 27 completed, building blocstagram

---
+++ Blocstagram/Blocstagram/ImagesTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Xoi's iMac on 2015-08-25.
+//  Copyright (c) 2015 XoiAHin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImagesTableViewController : UITableViewController
+
+
+@property (nonatomic, strong) NSMutableArray *images;
+
+
+@end
