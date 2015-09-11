3366d00f976892e6fe59a14770d2beccc537f314
Xoi Hin <xoihin@yahoo.com>
2015-09-11 12:11:49 -0400

    Assignment 42 completed, Accessing The Image Library

--- Blocstagram/Blocstagram/CropImageViewController.m
+++ Blocstagram/Blocstagram/CropImageViewController.m
@@ -19,6 +19,11 @@
 @property (nonatomic, strong) CropBox *cropBox;
 @property (nonatomic, assign) BOOL hasLoadedOnce;
 
+@property (nonatomic, strong) UIToolbar *topView;
+@property (nonatomic, strong) UIToolbar *bottomView;
+
+
+
 @end
 
 
@@ -45,9 +50,21 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
+    self.topView = [UIToolbar new];
+    self.bottomView = [UIToolbar new];
+    UIColor *whiteBG = [UIColor colorWithWhite:1.0 alpha:.15];
+    self.topView.barTintColor = whiteBG;
+    self.bottomView.barTintColor = whiteBG;
+    self.topView.alpha = 0.5;
+    self.bottomView.alpha = 0.5;
+
+    
     self.view.clipsToBounds = YES;
     
     [self.view addSubview:self.cropBox];
+    
+    [self.view addSubview:self.topView];
+    [self.view addSubview:self.bottomView];
     
     UIBarButtonItem *rightButton = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Crop", @"Crop command") style:UIBarButtonItemStyleDone target:self action:@selector(cropPressed:)];
     
@@ -65,6 +82,15 @@
 - (void) viewWillLayoutSubviews {
     [super viewWillLayoutSubviews];
     
+    CGFloat width = CGRectGetWidth(self.view.bounds);
+    self.topView.frame = CGRectMake(0, self.topLayoutGuide.length, width, 44);
+    
+    CGFloat yOriginOfBottomView = CGRectGetMaxY(self.topView.frame) + width;
+    CGFloat heightOfBottomView = CGRectGetHeight(self.view.frame) - yOriginOfBottomView;
+    self.bottomView.frame = CGRectMake(0, yOriginOfBottomView, width, heightOfBottomView);
+
+    
+    
     CGRect cropRect = CGRectZero;
     
     CGFloat edgeSize = MIN(CGRectGetWidth(self.view.frame), CGRectGetHeight(self.view.frame));
@@ -72,7 +98,9 @@
     
     CGSize size = self.view.frame.size;
     
-    self.cropBox.frame = cropRect;
+//    self.cropBox.frame = cropRect;
+    self.cropBox.frame = CGRectMake(0, CGRectGetMaxY(self.topView.frame), edgeSize, edgeSize);
+    
     self.cropBox.center = CGPointMake(size.width / 2, size.height / 2);
     self.scrollView.frame = self.cropBox.frame;
     self.scrollView.clipsToBounds = NO;
